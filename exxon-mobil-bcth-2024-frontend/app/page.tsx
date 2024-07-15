f7df0abd527d925adfdeb8608522cf24b15507cc
Thanapon Wongprasert <thanaponwps@gmail.com>
2024-07-15 23:23:31 +0700

    fix notice

--- exxon-mobil-bcth-2024-frontend/app/page.tsx
+++ exxon-mobil-bcth-2024-frontend/app/page.tsx
@@ -109,23 +109,23 @@ export default function Home() {
         </div>
         {/* AI */}
         <div
-            className="hidden md:flex mx-auto p-2 bg-[#f5f5f5] items-center text-[#737373] leading-none lg:rounded-full lg:inline-flex"
+            className="text-sm hidden md:flex mx-auto p-2 bg-[#f5f5f5] items-center text-[#737373] leading-none lg:rounded-full lg:inline-flex"
             role="alert"
           >
             <span className="flex rounded-full bg-[#d4d4d4] uppercase px-2 py-1 text-xs font-bold mr-3">
               NOTICE
             </span>
-            <span className="font-semibold mr-2 text-left flex-auto">
+            <span className="mr-2 text-left flex-auto">
               You're chatting with an AI. Please note, the information provided
               may not be accurate.
             </span>
-            <svg
+            {/* <svg
               className="fill-current opacity-75 h-4 w-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
             >
               <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
-            </svg>
+            </svg> */}
           </div>
           {/*  */}
         <div>
